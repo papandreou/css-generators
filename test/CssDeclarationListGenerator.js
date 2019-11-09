dcae7b71e5c0f5d6504b5aa726394061582cdfd9
Andreas Lind <andreaslindpetersen@gmail.com>
2019-11-09 13:49:55 +0100

    Update snapshot

--- test/CssDeclarationListGenerator.js
+++ test/CssDeclarationListGenerator.js
@@ -11,16 +11,16 @@ describe('CssDeclarationListGenerator', () => {
 
   it('should generate a CSS declaration list', () => {
     expect(new CssDeclarationListGenerator().take(10), 'to equal snapshot', [
-      'place-content: unsafe right; padding-right: 60%;',
-      'column-span: none;',
-      '-ms-scroll-chaining: chained;',
-      'border-inline-end-style: groove; -webkit-appearance: progress-bar-value; margin-top: -887.1769vw; offset-rotate: -998.4425deg reverse; -webkit-overflow-scrolling: touch;',
-      "letter-spacing: -985.8674pc; -ms-filter: 'ted';",
-      'border-bottom-left-radius: 37%;',
-      'counter-increment: gemsibsof 45%; -ms-scroll-limit-y-max: auto;',
-      'text-size-adjust: 56%; resize: horizontal; border-end-end-radius: -804.6558ex; mask-type: luminance; column-rule-color: cornflowerblue;',
-      "font-language-override: 'ik'; block-overflow: clip; mask-border-slice: 31% fill; clip-path: stroke-box;",
-      '-ms-hyphenate-limit-chars: 85%;'
+      'place-content: unsafe flex-start safe left; -webkit-line-clamp: none;',
+      '-moz-border-bottom-colors: hsla(1821526110240768 , 14% , 71% , 2%);',
+      'translate: 94% -998.4425em 984.4232em;',
+      "border-color: #865670; -ms-content-zoom-chaining: none; -ms-filter: 'ted'; -ms-scrollbar-darkshadow-color: hsl(-4813963569135616 , 46% , 9% , 62%);",
+      'border-top-width: medium;',
+      'inset-block-start: 4%; scroll-padding-block-end: 17%; contain: none; -ms-content-zoom-limit-max: 95%; text-size-adjust: 56%;',
+      'bottom: -968.0675cm; backdrop-filter: grayscale(12%); left: 84%; -ms-hyphenate-limit-lines: no-limit; text-decoration: overline blink #2f529f;',
+      'translate: 45% 39% 789.6547vmin;',
+      'orphans: 85%; border-inline-color: rgb(19% , 4%); transition: 79.3843s 173.5023ms;',
+      'border-block-start: dotted thick; -webkit-mask-image: none; -ms-content-zoom-limit-min: 99%; clip: auto; box-flex: -5427455770755072;'
     ]);
   });
 });
