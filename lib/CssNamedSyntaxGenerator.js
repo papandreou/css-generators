papandreou/css-generators

commit 313b961c88382dfa9ed8ec3234f7bb70fd4533b7

Add support for numeric ranges with <number>

lib/CssNamedSyntaxGenerator.js
@@ -27,8 +27,18 @@ const CssSyntaxGenerator = require('./CssSyntaxGenerator');
 
 class CssNamedSyntaxGenerator extends Generator {
   constructor(name) {
+    let min, max;
+    // https://drafts.csswg.org/css-values-3/#numeric-ranges
+    const matchRangeSyntax = name.match(/^(\S+) \[([^\]-]+),([^\]]+)\]$/);
+    if (matchRangeSyntax) {
+      name = matchRangeSyntax[1];
+      min = parseFloat(matchRangeSyntax[2]);
+      max = parseFloat(matchRangeSyntax[3]);
+    }
     super(name);
     this.name = name;
+    this.min = min;
+    this.max = max;
   }
 
   generate(chance) {
@@ -42,7 +52,7 @@ class CssNamedSyntaxGenerator extends Generator {
       // https://drafts.csswg.org/css-syntax-3/#function-token-diagram
       return `${new CssNamedSyntaxGenerator('ident-token').generate(chance)} (`;
     } else if (this.name === 'number') {
-      return String(chance.integer());
+      return String(chance.integer({ min: this.min, max: this.max }));
     } else if (this.name === 'zero') {
       return '0';
     } else if (this.name === 'string' || this.name === 'string-token') {

test/CssNamedSyntaxGenerator.js
@@ -20,6 +20,13 @@ describe('CssNamedSyntaxGenerator', () => {
     ]);
   });
 
+  // https://drafts.csswg.org/css-values-3/#numeric-ranges
+  it('supports the bracketed range restriction syntax with <number>', () => {
+    expect(
+      new CssNamedSyntaxGenerator('number [10,14]').take(3)
+    ).toEqualSnapshot(['11', '13', '14']);
+  });
+
   it('supports a double bar combinator', () => {
     expect(
       new CssNamedSyntaxGenerator('side-or-corner').take(3)

commit db97fbbdcb1b79c438ab640e2efece1d7e108c69

Unbreak the facades (broken in 313b961)

lib/CssNamedSyntaxGenerator.js
@@ -28,12 +28,16 @@ const CssSyntaxGenerator = require('./CssSyntaxGenerator');
 class CssNamedSyntaxGenerator extends Generator {
   constructor(name) {
     let min, max;
-    // https://drafts.csswg.org/css-values-3/#numeric-ranges
-    const matchRangeSyntax = name.match(/^(\S+) \[([^\]-]+),([^\]]+)\]$/);
-    if (matchRangeSyntax) {
-      name = matchRangeSyntax[1];
-      min = parseFloat(matchRangeSyntax[2]);
-      max = parseFloat(matchRangeSyntax[3]);
+    // Avoid failing on new CssNamedSyntaxGenerator() which would break chance-generators'
+    // createGeneratorFacade function
+    if (typeof name === 'string') {
+      // https://drafts.csswg.org/css-values-3/#numeric-ranges
+      const matchRangeSyntax = name.match(/^(\S+) \[([^\]-]+),([^\]]+)\]$/);
+      if (matchRangeSyntax) {
+        name = matchRangeSyntax[1];
+        min = parseFloat(matchRangeSyntax[2]);
+        max = parseFloat(matchRangeSyntax[3]);
+      }
     }
     super(name);
     this.name = name;